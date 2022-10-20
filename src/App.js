d9a405f0d45d8c6dcff1836aab002da960c0fe9e
Shawn <lcorel86@gmail.com>
2022-10-19 20:47:07 -0500

    at list keys

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
+
         <Route path="/" element={<AllMeetupsPage />}></Route>
         <Route path="/new-meetup" element={<NewMeetupsPage />}></Route>
         <Route path="/favorites" element={<FavoritesPage />}></Route>
+
       </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
