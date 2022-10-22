99dc641bc1c0d3aba56886d901fcc578ab3fcac3
Shawn <lcorel86@gmail.com>
2022-10-21 20:48:01 -0500

    warm up course done

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+
 import styles from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 
+import FavoritesContext from "../../store/favorites-context";
+
+
 function MeetupItem(props) {
+    const favoritesCtx = useContext(FavoritesContext);
+
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler() {
+        if (itemIsFavorite) {
+            favoritesCtx.removeFavorite(props.id);
+        } else {
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address
+            });
+        }
+    }
 
     return (
         <li className={styles.item}>
@@ -12,10 +34,12 @@ function MeetupItem(props) {
                 <div className={styles.content}>
                     <h3>{props.title}</h3>
                     <address>{props.address}</address>
-                    <p>{props.descreption}</p>
+                    <p>{props.description}</p>
                 </div>
                 <div className={styles.actions}>
-                    <button>To Favorites</button>
+                    <button onClick={toggleFavoriteStatusHandler}>
+                        { itemIsFavorite ? "Remove from Favorites" : "To Favorites" }
+                    </button>
                 </div>
             </Card>
         </li>
--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -11,7 +11,7 @@ function MeetupList(props) {
                     image={meetup.image}
                     title={meetup.title}
                     address={meetup.address}
-                    description={meetup.address}
+                    description={meetup.description}
                 />
             ))}
         </ul>
